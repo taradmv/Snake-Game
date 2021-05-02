e4b1f48154534f943d97fef92d0732bdb554c817
tara <tara.damavandi94@gmail.com>
2021-05-02 20:35:11 +0430

    updated

--- src/TheShapeOfSnake.java
+++ src/TheShapeOfSnake.java
@@ -9,36 +9,46 @@ import java.awt.event.KeyListener;
 public class TheShapeOfSnake extends JPanel implements ActionListener, KeyListener {
     Timer timer = new Timer(5, this);
     int x = 0, y = 0, velX = 0, velY = 0;
+    int snakeWidth = 25 , snakeHeight=25;
+    int n, m;
 
     public TheShapeOfSnake() {
         timer.start();
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.green);
+        g.fillRect(x, y, 10, 10);
+        int n = (int) (Math.random() * 375);
+        int m = (int) (Math.random() * 350);
         g.setColor(Color.red);
-        g.fillRect(x, y, 25, 25);
+        g.fillOval(n, m, 10, 10);
     }
+
 
     public void actionPerformed(ActionEvent e) {
         if (x < 0) {
             velX = 0;
             x = 0;
         }
-        if (x > 360) {
+        if (x > 375) {
             velX = 0;
-            x = 360;
+            x = 375;
         }
         if (y < 0) {
             velY = 0;
             y = 0;
         }
-        if (y > 335) {
+        if (y > 350) {
             velY = 0;
-            y = 335;
+            y = 350;
         }
+
         x = x + velX;
         y = y + velY;
         repaint();
@@ -79,7 +89,6 @@ public class TheShapeOfSnake extends JPanel implements ActionListener, KeyListen
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.add(obj);
     }
-
 
 
 }
